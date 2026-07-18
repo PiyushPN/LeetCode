c46acd065b2e2eda675e83dd067246982aa5b980
Piyush Mishra <45036676+PiyushPN@users.noreply.github.com>
2026-07-18 20:04:24 +0530

    Create 22-GenerateParentheses

---
+++ 22-GenerateParentheses/22-GenerateParentheses.cpp
@@ -0,0 +1,23 @@
+// Last updated: 18/07/2026, 20:04:24
+class Solution {
+public:
+    void solve(int open, int close, vector<string> &ans, string temp){
+        if(open==0 and close==0){
+            ans.push_back(temp);
+            return;
+        }
+        if(open>0){
+            solve(open-1, close, ans, temp+'(');
+        }
+        if(close>open){
+            solve(open, close-1, ans, temp+')');
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        int open=n, close=n;
+        string temp="";
+        solve(open, close, ans, temp);
+        return ans;
+    }
+};

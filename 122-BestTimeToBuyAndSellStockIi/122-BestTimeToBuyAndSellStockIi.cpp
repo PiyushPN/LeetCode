2ecaaa84fddca869c8d0c4399afe79a633b0c32a
Piyush Mishra <45036676+PiyushPN@users.noreply.github.com>
2026-07-18 20:03:09 +0530

    Create 122-BestTimeToBuyAndSellStockIi

---
+++ 122-BestTimeToBuyAndSellStockIi/122-BestTimeToBuyAndSellStockIi.cpp
@@ -0,0 +1,30 @@
+// Last updated: 18/07/2026, 20:03:08
+class Solution {
+public:
+    int dp[30001][2];
+    int solve(int i, int hold, vector<int> &prices, int n)
+    {
+        if(i>=n)
+        {
+            return 0;
+        }
+        if(dp[i][hold] != -1)
+        {
+            return dp[i][hold];
+        }
+        if(hold == 1)
+        {
+            return dp[i][hold] = max(solve(i+1, 0, prices, n)+prices[i], solve(i+1, 1, prices, n));
+        }
+        else
+        {
+            return dp[i][hold] = max(solve(i+1, 1, prices, n)-prices[i], solve(i+1, 0, prices, n));
+        }
+    }
+    int maxProfit(vector<int>& prices) {
+        memset(dp, -1, sizeof(dp));
+        int n = prices.size();
+        return solve(0, 0, prices, n);
+        
+    }
+};

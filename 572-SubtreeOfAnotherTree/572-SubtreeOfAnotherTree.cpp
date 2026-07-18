e0b762c4cc77b75e6511b192f481fa0dd48d352e
Piyush Mishra <45036676+PiyushPN@users.noreply.github.com>
2026-07-18 19:59:46 +0530

    Create 572-SubtreeOfAnotherTree

---
+++ 572-SubtreeOfAnotherTree/572-SubtreeOfAnotherTree.cpp
@@ -0,0 +1,39 @@
+// Last updated: 18/07/2026, 19:59:46
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool solve(TreeNode* root, TreeNode* subRoot){
+        if(root==NULL and subRoot==NULL){
+            return true;
+        }
+        if(root==NULL or subRoot==NULL){
+            return false;
+        }
+        if(root->val!=subRoot->val){
+            return false;
+        }
+        return solve(root->left, subRoot->left) and solve(root->right, subRoot->right);
+    }
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        if(!root){
+            return false;
+        }
+        if(root->val==subRoot->val){
+            if(solve(root, subRoot)){
+                return true;
+            }
+        }
+        return isSubtree(root->left, subRoot) or isSubtree(root->right, subRoot) ;
+
+    }
+};

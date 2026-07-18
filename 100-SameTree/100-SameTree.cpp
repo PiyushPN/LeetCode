175de92088d8b1bcd2df9ff829e2a57bf02e52d8
Piyush Mishra <45036676+PiyushPN@users.noreply.github.com>
2026-07-18 20:03:38 +0530

    Create 100-SameTree

---
+++ 100-SameTree/100-SameTree.cpp
@@ -0,0 +1,27 @@
+// Last updated: 18/07/2026, 20:03:38
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(!p and !q){
+            return true;
+        }
+        if(!p or !q){
+            return false;
+        }
+        if(p->val != q->val){
+            return false;
+        }
+        return isSameTree(p->left, q->left) and isSameTree(p->right, q->right);    
+    }
+};

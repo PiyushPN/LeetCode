a9e5236ecd5184ede92b5fabda19b96cb995f824
Piyush Mishra <45036676+PiyushPN@users.noreply.github.com>
2026-07-18 20:02:50 +0530

    Create 137-SingleNumberIi

---
+++ 137-SingleNumberIi/137-SingleNumberIi.cpp
@@ -0,0 +1,16 @@
+// Last updated: 18/07/2026, 20:02:50
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int x1 = 0, x2 = 0, mask = 0;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            x2 ^= x1 & nums[i];
+            x1 ^= nums[i];
+            mask = ~(x1 & x2);
+            x2 &= mask;
+            x1 &= mask;
+        }
+        return x1;
+    }
+};

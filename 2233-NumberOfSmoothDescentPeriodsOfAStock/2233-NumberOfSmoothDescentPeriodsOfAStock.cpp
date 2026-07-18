d1a674c5bda6d59af4ceaf65c9a6e7d4bafaa69e
Piyush Mishra <45036676+PiyushPN@users.noreply.github.com>
2026-07-18 19:57:35 +0530

    Create 2233-NumberOfSmoothDescentPeriodsOfAStock

---
+++ 2233-NumberOfSmoothDescentPeriodsOfAStock/2233-NumberOfSmoothDescentPeriodsOfAStock.cpp
@@ -0,0 +1,24 @@
+// Last updated: 18/07/2026, 19:57:34
+class Solution {
+public:
+    long long getDescentPeriods(vector<int>& prices) {
+        long long ans = 0;
+        long long temp = 0;
+        for(int i=1; i<prices.size(); i++)
+        {
+            if(prices[i - 1] - prices[i] == 1)
+            {
+                temp++;
+                ans += temp;
+            }
+            else
+            {
+                temp = 0;
+            }
+        }
+        ans += prices.size();
+        return ans;
+        
+        
+    }
+};

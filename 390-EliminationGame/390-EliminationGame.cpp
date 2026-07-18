be88da99d03c07d9034b2305e16f0e8106a440cf
Piyush Mishra <45036676+PiyushPN@users.noreply.github.com>
2026-07-18 20:00:12 +0530

    Create 390-EliminationGame

---
+++ 390-EliminationGame/390-EliminationGame.cpp
@@ -0,0 +1,23 @@
+// Last updated: 18/07/2026, 20:00:12
+class Solution {
+public:
+    int lastRemaining(int n) {
+        int head = 1, step = 1;
+        bool dir = true;
+        while(n>1)
+        {
+            if(dir)
+            {
+                head += step;
+            }
+            else
+            {
+                head += n%2==0?0:step;
+            }
+            step*=2;
+            n/=2;
+            dir = !dir;
+        }
+        return head;
+    }
+};

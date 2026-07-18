663d4266d82f7025303770a6ba331a32da03a7d8
Piyush Mishra <45036676+PiyushPN@users.noreply.github.com>
2026-07-18 19:59:41 +0530

    Create 645-SetMismatch

---
+++ 645-SetMismatch/645-SetMismatch.cpp
@@ -0,0 +1,23 @@
+// Last updated: 18/07/2026, 19:59:40
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        vector<int> ans;
+        int n = nums.size();
+        for(int i=0; i<n; i++){
+            if(nums[abs(nums[i])-1] < 0){
+                ans.push_back(abs(nums[i]));
+            }
+            else{
+                nums[abs(nums[i])-1]*=-1;
+            }
+        }
+        for(int i=0; i<n; i++){
+            if(nums[i]>0){
+                ans.push_back(i+1);
+                break;
+            }
+        }
+        return ans;
+    }
+};

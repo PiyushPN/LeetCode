7350be774558ef3307a6055d40978022fe2744d5
Piyush Mishra <45036676+PiyushPN@users.noreply.github.com>
2026-07-18 19:58:31 +0530

    Create 1568-PseudoPalindromicPathsInABinaryTree

---
+++ 1568-PseudoPalindromicPathsInABinaryTree/1568-PseudoPalindromicPathsInABinaryTree.cpp
@@ -0,0 +1,41 @@
+// Last updated: 18/07/2026, 19:58:30
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int ans = 0;
+    unordered_map<int,int> mp;
+    void solve(TreeNode *root){
+        if(!root){
+            return;
+        }
+        mp[root->val]++;
+        if(!root->left and !root->right){
+            int odd = 0;
+            for(auto ele: mp){
+                if(ele.second%2 == 1){
+                    odd++;
+                }
+            }
+            if(odd<=1){
+                ans++;
+            }
+        }
+        solve(root->left);
+        solve(root->right);
+        mp[root->val]--;
+    }
+    int pseudoPalindromicPaths (TreeNode* root) {
+        solve(root);
+        return ans;
+    }
+};

d1c074ea5f4f59cef547ab44d635050a9e54f898
Piyush Mishra <45036676+PiyushPN@users.noreply.github.com>
2026-07-18 19:59:36 +0530

    Create 692-TopKFrequentWords

---
+++ 692-TopKFrequentWords/692-TopKFrequentWords.cpp
@@ -0,0 +1,33 @@
+// Last updated: 18/07/2026, 19:59:35
+class Solution {
+public:
+    static bool cmp(pair<string, int> &a, pair<string, int> &b)
+    {
+        if(a.second != b.second)
+        {
+            return a.second > b.second;
+        }
+        return a.first < b.first;
+    }
+    vector<string> topKFrequent(vector<string>& words, int k) 
+    {
+        vector<string> ans;
+        unordered_map<string, int> mp;
+        for(int i = 0; i < words.size(); i++)
+        {
+            mp[words[i]]++;
+        }
+        vector<pair<string, int>> v;
+        for(auto it: mp)
+        {
+            v.push_back(it);
+        }
+        sort(v.begin(), v.end(), cmp);
+        for(int i = 0; i < k; i++)
+        {
+            ans.push_back(v[i].first);
+        }
+        
+        return ans;
+    }
+};

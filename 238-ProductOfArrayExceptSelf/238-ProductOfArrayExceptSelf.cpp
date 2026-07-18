627b019a937fed7287e4c88067c76c95a3ac132e
Piyush Mishra <45036676+PiyushPN@users.noreply.github.com>
2026-07-18 20:01:20 +0530

    Create 238-ProductOfArrayExceptSelf

---
+++ 238-ProductOfArrayExceptSelf/238-ProductOfArrayExceptSelf.cpp
@@ -0,0 +1,22 @@
+// Last updated: 18/07/2026, 20:01:20
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> pre(n), post(n), ans(n);
+        pre[0] = nums[0];
+        post[n-1] = nums[n-1];
+        for(int i=1; i<n; i++){
+            pre[i]=pre[i-1]*nums[i];
+        }
+        for(int i=n-2; i>=0; i--){
+            post[i]=post[i+1]*nums[i];
+        }
+        ans[0]=post[1];
+        ans[n-1]=pre[n-2];
+        for(int i=1; i<=n-2; i++){
+            ans[i]=pre[i-1]*post[i+1];
+        }
+        return ans;
+    }
+};

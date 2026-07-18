dd141cc6f3703f706727c3843367c07e3d95d014
Piyush Mishra <45036676+PiyushPN@users.noreply.github.com>
2026-07-18 19:59:15 +0530

    Create 948-SortAnArray

---
+++ 948-SortAnArray/948-SortAnArray.cpp
@@ -0,0 +1,8 @@
+// Last updated: 18/07/2026, 19:59:15
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        return nums;
+    }
+};
